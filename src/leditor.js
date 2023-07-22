ff95971d7586aea0f73e512f4b7c748af11d17c6
ronaldaug <saisenfa@gmail.com>
2023-07-22 18:51:38 +0630

    Fix: add image resizer

--- src/leditor.js
+++ src/leditor.js
@@ -287,12 +287,235 @@ class Leditor extends LEHooks {
         })
     }
 
+
+    // https://stackoverflow.com/a/71180398/2134209
+    enableImageResizeInDiv() {
+        if (!(/chrome/i.test(navigator.userAgent) && /google/i.test(window.navigator.vendor))) {
+            return;
+        }
+        let resizing = false;
+        let currentImage;
+        const createDOM = function (elementType, className, styles) {
+            let ele = document.createElement(elementType);
+            ele.className = className;
+            setStyle(ele, styles);
+            return ele;
+        };
+        const setStyle = function (ele, styles) {
+            for (let key in styles) {
+                ele.style[key] = styles[key];
+            }
+            return ele;
+        };
+        const removeResizeFrame = ()  => {
+            document.querySelectorAll(".resize-frame,.resizer").forEach((item) => item.parentNode.removeChild(item));
+        };
+        const offset = (el)  => {
+            const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        };
+        const clickImage = (img) => {
+            removeResizeFrame();
+            currentImage = img;
+            const imgHeight = img.offsetHeight;
+            const imgWidth = img.offsetWidth;
+            const imgPosition = { top: img.offsetTop, left: img.offsetLeft };
+            const editorScrollTop  = this.leditor.scrollTop;
+            const editorScrollLeft = this.leditor.scrollLeft;
+            const top = imgPosition.top - editorScrollTop - 1;
+            const left = imgPosition.left - editorScrollLeft - 1;
+
+            this.leditor.append(createDOM('span', 'resize-frame', {
+                margin: '10px',
+                position: 'absolute',
+                top: (top + imgHeight - 10) + 'px',
+                left: (left + imgWidth - 10) + 'px',
+                borderRight: '4px solid #000',
+                borderBottom: '4px solid #000',
+                width: '8px',
+                height: '8px',
+                cursor: 'se-resize',
+                zIndex: 1
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer top-border', {
+                position: 'absolute',
+                top: (top) + 'px',
+                left: (left) + 'px',
+                border: 'dashed 1px grey',
+                width: imgWidth + 'px',
+                height: '0px'
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer left-border', {
+                position: 'absolute',
+                top: (top) + 'px',
+                left: (left) + 'px',
+                border: 'dashed 1px grey',
+                width: '0px',
+                height: imgHeight + 'px'
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer right-border', {
+                position: 'absolute',
+                top: (top) + 'px',
+                left: (left + imgWidth) + 'px',
+                border: 'dashed 1px grey',
+                width: '0px',
+                height: imgHeight + 'px'
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer bottom-border', {
+                position: 'absolute',
+                top: (top + imgHeight) + 'px',
+                left: (left) + 'px',
+                border: 'dashed 1px grey',
+                width: imgWidth + 'px',
+                height: '0px'
+            }));
+
+            document.querySelector('.resize-frame').onmousedown = () => {
+                resizing = true;
+                return false;
+            };
+
+            this.leditor.onmouseup = () => {
+                if (resizing) {
+                    currentImage.style.width = document.querySelector('.top-border').offsetWidth + 'px';
+                    currentImage.style.height = document.querySelector('.left-border').offsetHeight + 'px';
+                    refresh();
+                    currentImage.click();
+                    resizing = false;
+                }
+            };
+
+            this.leditor.onmousemove = (e) => {
+                if (currentImage && resizing) {
+                    let height = e.pageY - offset(currentImage).top;
+                    let width = e.pageX - offset(currentImage).left;
+                    height = height < 1 ? 1 : height;
+                    width = width < 1 ? 1 : width;
+                    const top = imgPosition.top - editorScrollTop - 1;
+                    const left = imgPosition.left - editorScrollLeft - 1;
+                    setStyle(document.querySelector('.resize-frame'), {
+                        top: (top + height - 10) + 'px',
+                        left: (left + width - 10) + "px"
+                    });
+
+                    setStyle(document.querySelector('.top-border'), { width: width + "px" });
+                    setStyle(document.querySelector('.left-border'), { height: height + "px" });
+                    setStyle(document.querySelector('.right-border'), {
+                        left: (left + width) + 'px',
+                        height: height + "px"
+                    });
+                    setStyle(document.querySelector('.bottom-border'), {
+                        top: (top + height) + 'px',
+                        width: width + "px"
+                    });
+                }
+                return false;
+            };
+        };
+        const bindClickListener = () => {
+            this.leditor.querySelectorAll('img').forEach((img, i) => {
+                img.onclick = (e) => {
+                    if (e.target === img) {
+                        clickImage(img);
+                    }
+                };
+            });
+        };
+        const refresh =  () => {
+            bindClickListener();
+            removeResizeFrame();
+            if (!currentImage) {
+                return;
+            }
+            const img = currentImage;
+            const imgHeight = img.offsetHeight;
+            const imgWidth = img.offsetWidth;
+            const imgPosition = { top: img.offsetTop, left: img.offsetLeft };
+            const editorScrollTop = this.leditor.scrollTop;
+            const editorScrollLeft = this.leditor.scrollLeft;
+            const top = imgPosition.top - editorScrollTop - 1;
+            const left = imgPosition.left - editorScrollLeft - 1;
+
+            this.leditor.append(createDOM('span', 'resize-frame', {
+                position: 'absolute',
+                top: (top + imgHeight) + 'px',
+                left: (left + imgWidth) + 'px',
+                border: 'solid 2px red',
+                width: '6px',
+                height: '6px',
+                cursor: 'se-resize',
+                zIndex: 1
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer', {
+                position: 'absolute',
+                top: (top) + 'px',
+                left: (left) + 'px',
+                border: 'dashed 1px grey',
+                width: imgWidth + 'px',
+                height: '0px'
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer', {
+                position: 'absolute',
+                top: (top) + 'px',
+                left: (left + imgWidth) + 'px',
+                border: 'dashed 1px grey',
+                width: '0px',
+                height: imgHeight + 'px'
+            }));
+
+            this.leditor.append(createDOM('span', 'resizer', {
+                position: 'absolute',
+                top: (top + imgHeight) + 'px',
+                left: (left) + 'px',
+                border: 'dashed 1px grey',
+                width: imgWidth + 'px',
+                height: '0px'
+            }));
+        };
+        const reset =  () => {
+            if (currentImage != null) {
+                currentImage = null;
+                resizing = false;
+                removeResizeFrame();
+            }
+            bindClickListener();
+        };
+        this.leditor.addEventListener('scroll', function () {
+            reset();
+        }, false);
+        this.leditor.addEventListener('mouseup', function (e) {
+            if (!resizing) {
+                const x = (e.x) ? e.x : e.clientX;
+                const y = (e.y) ? e.y : e.clientY;
+                let mouseUpElement = document.elementFromPoint(x, y);
+                if (mouseUpElement) {
+                    let matchingElement = null;
+                    if (mouseUpElement.tagName === 'IMG') {
+                        matchingElement = mouseUpElement;
+                    }
+                    if (!matchingElement) {
+                        reset();
+                    } else {
+                        clickImage(matchingElement);
+                    }
+                }
+            }
+        });
+    }
+
     highlightText() {
         if (!this.currentSelection.toString()) {
             Msg.error('Please select text!')
             return;
         }
-
 
         this.removeToolTip();
     }
@@ -324,6 +547,7 @@ class Leditor extends LEHooks {
         this.leditor.setAttribute('contenteditable', 'true');
         this.leditor.classList.add('active-edit');
         this.leditor.focus();
+        this.enableImageResizeInDiv();
 
         // Listen click event of tooltip buttons (call from body to be sure)
         document.body.addEventListener("click", e => {
@@ -356,17 +580,6 @@ class Leditor extends LEHooks {
             if (!this.tooltip.contains(e.target)) {
                 this.removeToolTip()
             }
-
-            // Listen everytime user hit backspace and enter
-            this.leditor.addEventListener('keydown', e => {
-                if (this.leditor.innerHTML == 0) {
-                    this.leditor.innerHTML = '<p>&nbsp;</p>'
-                }
-                if (e.keyCode == 8) {
-                    this.removeAllhidden();
-                }
-            })
-
 
         })
 
